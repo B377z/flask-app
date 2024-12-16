c241121c04def04e416ca744dc642ce692a3dca7
B377z <unwana.udo@flo-dojo.com>
2024-12-16 11:43:55 -0600

    Code to get tasks

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,30 @@ from flask import Flask
 
 app = Flask(__name__)
 
+tasks = [{
+    "id": 1,
+    "title": "Learn Flask",
+    "Completed": False
+    },
+    {
+    "id": 2,
+    "title": "Build a Web App",
+    "Completed": False
+    }]
+
+
 @app.route("/")
 def home():
     """My Home Page"""
     return "<h1>Welcome to the Task Manager API!</h1>"
 
-@app.route("/about")
+@app.route("/tasks")
 def about():
-    return "This is the About page."
+    """
+    Returns a list of tasks
+
+    """
+    return {"tasks": tasks}
 
 if __name__ == "__main__":
     app.run(debug=True)
